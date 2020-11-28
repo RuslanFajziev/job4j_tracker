1b50209f873908f1e088ce9bd98a2ec0250b7606
FaizievRT <rus23f@mail.ru>
2020-11-28 19:44:37 +0300

    5.1. Зона видимости переменных [#398937] 2

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,12 +8,17 @@ public class Item {
     }
 
     public Item(String name) {
+        this.name = name;
     }
 
     public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public static void main(String[] args) {
         Item item = new Item();
+        Item itemName = new Item("Vasay");
+        Item itemIdName = new Item(45, "Petay");
     }
 }

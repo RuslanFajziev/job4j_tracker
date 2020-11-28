68f664d39e16cbdef29a2d050f23c423c720862f
FaizievRT <rus23f@mail.ru>
2020-11-28 22:57:25 +0300

    7. Date. Отображение даты. [#398945]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created;
 
     public Item() {
     }
@@ -14,11 +18,18 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+        this.created = LocalDateTime.now();
     }
 
     public static void main(String[] args) {
         Item item = new Item();
         Item itemName = new Item("Vasay");
         Item itemIdName = new Item(45, "Petay");
+    }
+
+    public String getCreated() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = created.format(formatter);
+        return currentDateTimeFormat;
     }
 }

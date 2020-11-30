67649d3a57a39ac591895481acd3255e6c9c720f
FaizievRT <rus23f@mail.ru>
2020-11-30 21:16:03 +0300

    3. Реализация профессий в коде [#398947]

--- src/main/java/ru/job4j/profession/Engineer.java
+++ src/main/java/ru/job4j/profession/Engineer.java
@@ -1,14 +1,23 @@
-//package ru.job4j.profession;
-//
-//public class Engineer extends Profession {
-//    private int experience;
-//
-//    public int getExperience() {
-//        return this.experience;
-//    }
-//
-//    public static void main(String[] args) {
-//        Engineer nameEng = new Engineer();
-//        System.out.println(nameEng.getName());
-//    }
-//}
+package ru.job4j.profession;
+
+public class Engineer {
+
+    private int experience;
+
+    public Engineer(int experience) {
+        this.experience = experience;
+    }
+
+    public int getExperience() {
+        return this.experience;
+    }
+
+    public static void main(String[] args) {
+        Engineer nameEng = new Engineer(10);
+        System.out.println(nameEng.getExperience());
+    }
+
+    public String getInfo (String txt) {
+        return txt + this.experience;
+    }
+}
--- src/main/java/ru/job4j/profession/Programmer.java
+++ src/main/java/ru/job4j/profession/Programmer.java
@@ -1,9 +1,16 @@
-//package ru.job4j.profession;
-//
-//public class Programmer extends Engineer{
-//    private String languageCode;
-//    private Engineer engineer;
-//
-//    public Bytecode heal(CodeJava code) {
-//    }
-//}
+package ru.job4j.profession;
+
+public class Programmer extends Engineer {
+    private String languageCode;
+    private Engineer engineer;
+    private String info;
+
+    public Bytecode heal(CodeJava code) {
+    }
+
+    public static void main(String[] args) {
+        Engineer engineer = new Engineer(25);
+        String message = engineer.getInfo("This character has experience: ");
+        System.out.println(message);
+    }
+}

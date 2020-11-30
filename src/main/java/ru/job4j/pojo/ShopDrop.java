4c16f172ef4af0144616f86e816b2f7d939ada4f
FaizievRT <rus23f@mail.ru>
2020-11-30 22:42:19 +0300

    3. Реализация профессий в коде [#398947]

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -1,39 +1,39 @@
-//package ru.job4j.pojo;
-//
-//import java.util.Arrays;
-//
-//public class ShopDrop {
-//    public static void main(String[] args) {
-//        Product[] products = new Product[3];
-//        products[0] = new Product("Milk", 10);
-//        products[1] = new Product("Bread", 4);
-//        products[2] = new Product("Egg", 19);
-//        for (int i = 0; i < products.length; i++) {
-//            Product product = products[i];
-//            System.out.println(product.getName());
-//        }
-//        System.out.println();
-//        products[1] = products[2];
-//        products[2] = null;
-//        for (int i = 0; i < products.length; i++) {
-//            Product product = products[i];
-//            if (product != null) {
-//                System.out.println(product.getName());
-//            } else {
-//                System.out.println("null");
-//            }
-//        }
-//    }
-//
-//    public static Product[] leftShift(Product[] products, int index) {
-//        Product[] newArray = Arrays.copyOf(products, products.length);
-//        for (int i = index; i < products.length; i++) {
-//            if (i + 1 >= products.length) {
-//                newArray[i] = null;
-//            } else {
-//                newArray[i] = products[i + 1];
-//            }
-//        }
-//        return newArray;
-//    }
-//}
+package ru.job4j.pojo;
+
+import java.util.Arrays;
+
+public class ShopDrop {
+    public static void main(String[] args) {
+        Product[] products = new Product[3];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        products[2] = new Product("Egg", 19);
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            System.out.println(product.getName());
+        }
+        System.out.println();
+        products[1] = products[2];
+        products[2] = null;
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+    }
+
+    public static Product[] leftShift(Product[] products, int index) {
+        Product[] newArray = Arrays.copyOf(products, products.length);
+        for (int i = index; i < products.length; i++) {
+            if (i + 1 >= products.length) {
+                newArray[i] = null;
+            } else {
+                newArray[i] = products[i + 1];
+            }
+        }
+        return newArray;
+    }
+}

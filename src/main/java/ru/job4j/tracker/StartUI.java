79171d6d9a48781dd15df94966ec9a1bddcc0cfb
FaizievRT <rus23f@mail.ru>
2020-12-03 00:48:38 +0300

    9. toString [#398957]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,12 +2,16 @@ package ru.job4j.tracker;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
+//        Tracker tracker = new Tracker();
+//        Item item = new Item();
+//        tracker.add(item);
+//        Item item2 = new Item();
+//        tracker.add(item2);
+//        System.out.println(tracker.findById(1));
+//        System.out.println(tracker.findById(2));
         Item item = new Item();
-        tracker.add(item);
-        Item item2 = new Item();
-        tracker.add(item2);
-        System.out.println(tracker.findById(1));
-        System.out.println(tracker.findById(2));
+        item.setId(1);
+        item.setName("Boris");
+        System.out.println(item);
     }
 }

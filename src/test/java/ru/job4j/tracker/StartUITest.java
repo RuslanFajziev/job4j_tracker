210ba96e26ef792cc9523cd9a78d528ca1d3974d
FaizievRT <rus23f@mail.ru>
2020-12-17 01:19:20 +0300

    1.1. Тесты на StartUI. [#398903]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -128,4 +128,34 @@ public class StartUITest {
         str += "=== Exit Program ===" + System.lineSeparator() + "-------------------------" + System.lineSeparator();
         assertThat(out.toString(), is(str));
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"1", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitProgram(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "-------------------------%n"
+                        + "         Menu%n"
+                        + "-------------------------%n"
+                        + "0. === Exit Program ===%n"
+                        + "-------------------------%n"
+                        + "Wrong input, you can select: 0 .. 0%n"
+                        + "-------------------------%n"
+                        + "         Menu%n"
+                        + "-------------------------%n"
+                        + "0. === Exit Program ===%n"
+                        + "-------------------------%n"
+                        + "=== Exit Program ===%n"
+                        + "-------------------------%n"
+                )
+        ));
+    }
 }

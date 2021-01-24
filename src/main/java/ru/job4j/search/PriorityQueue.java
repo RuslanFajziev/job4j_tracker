ef9bf4c0564befce6c5a1da7222a23c1a1e7353a
FaizievRT <rus23f@mail.ru>
2021-01-25 00:58:48 +0300

    2. Travis CI [#398842] 2

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 /**
  * Класс описывает работу простейшей очереди по приоритету, которая работает
  * по принципу FIFO (first in - first out)
+ *
  * @author STAS KOROBEYNIKOV
  * @version 1.0
  */
@@ -18,13 +19,14 @@ public class PriorityQueue {
      * Метод принимает на вход заявку и добавляет ее в очередь.
      * Если встречаются 2 задания с одинаковым приоритетом, то в очереди
      * они распределяются по принципу FIFO.
+     *
      * @param task задача которая добавляется в очередь
      */
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
             if (element.getPriority() > task.getPriority()) {
-               break;
+                break;
             }
             index++;
         }
@@ -37,6 +39,7 @@ public class PriorityQueue {
 
     /**
      * Метод позволяет получить первую задачу в очереди
+     *
      * @return возвращает задачу из головы очереди или null если очередь пуста
      */
     public Task take() {

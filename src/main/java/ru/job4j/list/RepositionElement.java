7392684ec10661fdf3bd0811d3a74cc2d0aa82c1
FaizievRT <rus23f@mail.ru>
2021-02-05 00:47:14 +0300

    2. Отсортировать департаменты [#398847]

--- src/main/java/ru/job4j/list/RepositionElement.java
+++ src/main/java/ru/job4j/list/RepositionElement.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class RepositionElement {
     public static List<String> changePosition(List<String> list, int index) {
-        int sizeL = list.size() -1;
+        int sizeL = list.size() - 1;
         String value = list.remove(sizeL);
         if (index <= sizeL) {
             list.set(index, value);
